b81fb351252cb3a866adea99fe9ca10264eca055
jackw <jackw@DESKTOP-GR0IRF5>
2023-01-28 09:20:38 -0600

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WidmerGuessAWord2022
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.txtTries = new System.Windows.Forms.TextBox();
+            this.lblLettersUsed = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.chkSoundEffects = new System.Windows.Forms.CheckBox();
+            this.lblDefinition = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guess a letter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of tries:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Letters used:";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtGuess.Location = new System.Drawing.Point(118, 119);
+            this.txtGuess.MaxLength = 1;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(49, 22);
+            this.txtGuess.TabIndex = 3;
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGuess.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtGuess_KeyDown);
+            // 
+            // txtTries
+            // 
+            this.txtTries.Location = new System.Drawing.Point(118, 168);
+            this.txtTries.Name = "txtTries";
+            this.txtTries.ReadOnly = true;
+            this.txtTries.Size = new System.Drawing.Size(49, 22);
+            this.txtTries.TabIndex = 4;
+            this.txtTries.TabStop = false;
+            // 
+            // lblLettersUsed
+            // 
+            this.lblLettersUsed.AutoSize = true;
+            this.lblLettersUsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLettersUsed.Location = new System.Drawing.Point(113, 272);
+            this.lblLettersUsed.Name = "lblLettersUsed";
+            this.lblLettersUsed.Size = new System.Drawing.Size(64, 25);
+            this.lblLettersUsed.TabIndex = 5;
+            this.lblLettersUsed.Text = "label4";
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(220, 122);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(107, 68);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(12, 72);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(130, 23);
+            this.lblWord.TabIndex = 7;
+            this.lblWord.Text = "hiddenword";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 226);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(66, 16);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "status line";
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(375, 122);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(112, 68);
+            this.btnPlayAgain.TabIndex = 9;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Player Name:";
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Location = new System.Drawing.Point(108, 28);
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(166, 22);
+            this.txtPlayerName.TabIndex = 11;
+            // 
+            // chkSoundEffects
+            // 
+            this.chkSoundEffects.AutoSize = true;
+            this.chkSoundEffects.Location = new System.Drawing.Point(303, 28);
+            this.chkSoundEffects.Name = "chkSoundEffects";
+            this.chkSoundEffects.Size = new System.Drawing.Size(111, 20);
+            this.chkSoundEffects.TabIndex = 12;
+            this.chkSoundEffects.Text = "Sound Effects";
+            this.chkSoundEffects.UseVisualStyleBackColor = true;
+            // 
+            // lblDefinition
+            // 
+            this.lblDefinition.AutoSize = true;
+            this.lblDefinition.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDefinition.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblDefinition.Location = new System.Drawing.Point(13, 338);
+            this.lblDefinition.Name = "lblDefinition";
+            this.lblDefinition.Size = new System.Drawing.Size(189, 23);
+            this.lblDefinition.TabIndex = 13;
+            this.lblDefinition.Text = "Definition goes here...";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 407);
+            this.Controls.Add(this.lblDefinition);
+            this.Controls.Add(this.chkSoundEffects);
+            this.Controls.Add(this.txtPlayerName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblLettersUsed);
+            this.Controls.Add(this.txtTries);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Guess A Word";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.TextBox txtTries;
+        private System.Windows.Forms.Label lblLettersUsed;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.CheckBox chkSoundEffects;
+        private System.Windows.Forms.Label lblDefinition;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,369 @@
+﻿// Jack Widmer
+// 11/23/2022
+// GuessAWord with online words.
+
+/*
+ * In Chapter 6, you created a game named GuessAWord in which a player
+ * guesses letters to try to replicate a hidden word. Now create a
+ * modified version of the program named GuessAWordWithExceptionHandling
+ * that throws and catches an exception when the user enters a guess that
+ * is not a letter of the alphabet. Create a NonLetterException class tha
+ * t descends from Exception. The Message string for your new class should
+ * indicate the nonletter character that caused the Exception's creation.
+ * When a NonLetterException is thrown and caught, the message should be
+ * displayed.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media; // needed for sound player
+using System.IO; // Needed for file I/O
+using System.Net; // needed for WebClient
+using System.Runtime.CompilerServices;
+
+namespace WidmerGuessAWord2022
+{
+    public partial class frmMain : Form
+    {
+        //string[] words = {"powershell", "computer", "network", "linux",
+        //                  "microsoft", "desktop", "keyboard", "hello"};
+
+        // This used to be an array declaration with predefined words.
+        // We now changed it to a list of type string
+        List<string> words = new List<string>();
+
+        Random random = new Random();
+
+        string word;
+        string definition;
+        string hiddenWord;
+        int countTries;
+        
+
+        // Add the crowd5.wav file to resources through project, project
+        // properties, then resources. Add resource through existing file
+        SoundPlayer audio = new SoundPlayer(WidmerGuessAWord2022.Properties
+                                            .Resources.crowd5);
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // Note: maxLength property set to 1 on txtGuess
+
+            string myLetter;
+            char[] arrayHiddenWord = hiddenWord.ToCharArray();
+            //myLetter = txtGuess.Text;
+            try
+            {
+                myLetter = GetLetter(txtGuess.Text);
+                if (lblLettersUsed.Text.Contains(myLetter))
+                {
+                    lblStatus.Text = "You have already used that letter!";
+                }
+                else
+                { // Letter has not been used yet
+                    lblLettersUsed.Text += myLetter;
+                    countTries++;
+
+                    bool found = false;
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word.Substring(i, 1) == myLetter)
+                        { // A match was found. Replace the * char with the letter
+                            arrayHiddenWord[i] = Convert.ToChar(myLetter);
+                            found = true;
+                        }
+                    }
+                    if (found)
+                    {
+                        lblStatus.Text = "Clap clap clap";
+                        if(chkSoundEffects.Checked) audio.Play();
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Sigh :-(";
+                    }
+
+                    hiddenWord = new string(arrayHiddenWord);
+                    lblWord.Text = hiddenWord;
+                    txtTries.Text = countTries.ToString();
+
+                    // Determine if the entire word has been guessed
+                    if (!hiddenWord.Contains("*"))
+                    {
+                        lblStatus.Text = "You solved the word!";
+                        btnGuess.Enabled = false;
+                        btnPlayAgain.Visible = true;
+                    }
+
+                    txtGuess.Text = "";
+                    txtGuess.Focus();
+                }
+            } // End try
+            catch(NonLetterException ex)
+            {
+                lblStatus.Text = "You have a bad character: " + ex.OffendingCharacter;
+            }
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Read in our initial set up backup word list
+            words.Add("powershell");
+            words.Add("computer");
+            words.Add("network");
+            words.Add("linux");
+            words.Add("microsoft");
+            words.Add("desktop");
+            words.Add("keyboard");
+            words.Add("hello");
+
+            const string FILENAME = "english_words_basic.txt";
+            string lineIn;
+            int lineCounter = 0; // Not really needed. Used for debugging
+
+            try
+            {
+                FileStream dictionaryFile = new FileStream(FILENAME, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(dictionaryFile);
+
+                dictionaryFile.Position = 0;
+                lineCounter = 0;
+
+                lineIn = reader.ReadLine();
+                while (lineIn != null)
+                {
+                    words.Add(lineIn.ToLower());
+                    lineCounter++;
+                    lineIn = reader.ReadLine();
+                }
+
+                dictionaryFile.Close();
+                reader.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("Dictionary not found.", "Reduced Functionality", 
+                                 MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            Console.WriteLine("lineCounter = " + lineCounter);
+
+            // Read user preferences to a file...
+            const string FILENAMEPREF = "preferences.txt";
+
+            try
+            {
+                FileStream preferencesFile = new FileStream(FILENAMEPREF, FileMode.Open, FileAccess.Read);
+                StreamReader readerPref = new StreamReader(preferencesFile);
+
+                txtPlayerName.Text = readerPref.ReadLine();
+                chkSoundEffects.Checked = Convert.ToBoolean(readerPref.ReadLine());
+
+                preferencesFile.Close();
+                readerPref.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("Preferences not set..", "Preferences Error",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            NewGame();
+        } // frmMain_Load end
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Write user preferences to a file...
+            const string FILENAMEPREF = "preferences.txt";
+
+            FileStream preferencesFile = new FileStream(FILENAMEPREF, FileMode.Create, FileAccess.Write);
+            StreamWriter writerPref = new StreamWriter(preferencesFile);
+
+            writerPref.WriteLine(txtPlayerName.Text);
+            writerPref.WriteLine(Convert.ToString(chkSoundEffects.Checked));
+
+            writerPref.Close();
+            preferencesFile.Close();
+
+            string csvFile = "runningLog.csv";
+            string playerDetails = Environment.NewLine + txtPlayerName.Text + "," + DateTime.Now.ToString("h:mm:ss tt") + 
+                "," + countTries.ToString() + "," + word;
+
+            if (!File.Exists(csvFile))
+            {
+                string header = "Player Name" + "," + "Date/Time" + "," + "Attempts" + "," + "Word";
+                File.WriteAllText(csvFile, header);
+            }
+
+            File.AppendAllText(csvFile, playerDetails);
+        }
+
+        private void NewGame()
+        {
+            // word = words[random.Next(words.Length)];
+            // The above code was for an array. Below is the new way!
+            word = words[random.Next(words.Count)];
+
+            // ONLINE retrieval of a word
+            // Go out to the web and get a word
+            try
+            {
+                string htmlContent;
+                Console.WriteLine("About to open website.");
+                using (WebClient client = new WebClient())
+                {
+                    string url = "https://randomword.com";
+                    htmlContent = client.DownloadString(url);
+                    // Console.WriteLine(htmlContent);
+                }
+
+                // <div id="random_word">quinism</div>
+                string randomWordStart = @"<div id=""random_word"">";
+                int startWordIndex = htmlContent.IndexOf(randomWordStart) + 22;
+                int endWordIndex = htmlContent.IndexOf("</div>", startWordIndex);
+                Console.WriteLine("startWordIndex = " + startWordIndex);
+                Console.WriteLine("endWordIndex = " + endWordIndex);
+
+                if (htmlContent.IndexOf(randomWordStart) != -1)
+                {
+                    word = htmlContent.Substring(startWordIndex, endWordIndex - startWordIndex);
+                    Console.WriteLine("word = " + word);
+
+
+                    //<div id="random_word_definition">pathological state resulting from excessive use of quinine  </div>
+
+                    string randomDefinitionStart = @"<div id=""random_word_definition"">";
+                    int startDefinitionIndex = htmlContent.IndexOf(randomDefinitionStart) + 33;
+                    int endDefinitionIndex = htmlContent.IndexOf("</div>", startDefinitionIndex);
+                    Console.WriteLine("startDefinitionIndex = " + startDefinitionIndex);
+                    Console.WriteLine("endDefinitionIndex = " + endDefinitionIndex);
+
+                    if (htmlContent.IndexOf(randomDefinitionStart) != -1)
+                    {
+                        definition = htmlContent.Substring(startDefinitionIndex, endDefinitionIndex - startDefinitionIndex);
+                        Console.WriteLine("definition = " + definition);
+                        lblDefinition.Text = definition;
+                    }
+                    else
+                    {
+                        lblDefinition.Text = "Error finding online definition.";
+                    }
+                }
+                else
+                {
+                    lblDefinition.Text = "Error finding online word";
+                }
+
+            } // end try
+            catch(WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+                lblDefinition.Text = "Online dictionary not found.";
+            }
+            hiddenWord = "";
+            // Create the hiddenWord with the same number of asterisks
+            // as the chosen word.
+            for(int i = 0; i < word.Length; i++)
+                hiddenWord += "*";
+
+            lblWord.Text = hiddenWord;
+
+            lblStatus.Text = "";
+            txtGuess.Text = "";
+            countTries = 0;
+            txtTries.Text = "";
+
+            lblLettersUsed.Text = "";
+            btnPlayAgain.Visible = false;
+            btnGuess.Enabled = true;
+
+        }
+
+        private void txtGuess_KeyDown(object sender, KeyEventArgs e)
+        { // See if the enter key was pressed in the letter guess text box
+            if(e.KeyCode == Keys.Enter)
+            {
+                btnGuess_Click(this, new EventArgs());
+            }
+        }
+
+        private string GetLetter(string myString)
+        {
+            // Sanitize the input: make sure we have only a single character
+            // a-z.
+            char letter;
+            if (Char.TryParse(myString, out letter))
+            {
+                // We have exactly one character
+                letter = Char.ToLower(letter);
+                if(letter < 'a' || letter > 'z')
+                {
+                    // Problem...
+                    string badLetter = Convert.ToString(letter);
+                    throw new NonLetterException(badLetter);
+                }
+
+                string goodletter = Convert.ToString(letter);
+                return goodletter;
+            }
+            else
+            {
+                // TryParse failed
+                throw new NonLetterException("Null or multiple characters.");
+            }
+        }
+
+        
+    } // End public partial class frmMain : Form
+
+    // This is the custom exception class that the book
+    // exercise is asking us to create
+
+    public class NonLetterException : Exception
+    {
+        private string offendingCharacter;
+
+        public NonLetterException() : base("A non-letter was entered.")
+        {
+            // Nothing else we need to do...
+        }
+
+        public NonLetterException(string nonletter)
+            : base("The non-letter '" + nonletter + "' was entered.")
+        {
+            offendingCharacter = nonletter;
+        }
+
+        // Using built in getter
+        public string OffendingCharacter
+        {
+            get { return offendingCharacter; }
+        }
+        // We did it like this the last couple weeks...
+        /*
+        public string GetOffendingCharacter()
+        {
+            return offendingCharacter;
+        }
+        */
+    } // end public class NonLetterException : Exception
+
+ 
+}
